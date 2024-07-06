3bbd506c7cfa116052ac63116ba0af887f152494
Tiles <stefan@thetilesgame.com>
2024-07-06 19:40:15 +0100

    Remove extra params

--- constants.js
+++ constants.js
@@ -2,8 +2,7 @@ const PARAMS = Object.freeze({
     SUPABASE_JWT_LIFETIME: 3600,
     SUPABASE_ROLE: "claim_role",
     SUPABASE_USERS_TABLE: "users",
-    SUPABASE_SCHEMA: "custom_schema",
-    REDIS_LOCK_TIME: 600
+    SUPABASE_SCHEMA: "custom_schema"
 });
 
 module.exports = {
